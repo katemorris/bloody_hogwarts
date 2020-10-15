394822fe0fdae562d59319e01d6ec4197d37e35a
Kate Tester <katemorris@gmail.com>
2020-10-15 12:43:56 -0600

    Adds the test for student show page

---
+++ spec/features/students/show_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor' do
+  describe 'when I visit the each student page' do
+    before(:each) do
+      @harry = Student.create(name: 'Harry Potter', age: 14, house: 'Gryffindor')
+      @dada = Course.create(name: 'Defense Against the Dark Arts')
+      @potions = Course.create(name: 'Potions')
+
+    end
+    it "I see a list of the student's courses" do
+      visit "/students/#{@harry.id}"
+
+      expect(page).to have_content(@dada.name)
+      expect(page).to have_content(@potions.name)
+    end
+  end
+end
