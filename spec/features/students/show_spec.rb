27d865a3da9f4d328dce71aa5ca8687347f4a8a8
Kate Tester <katemorris@gmail.com>
2020-10-15 12:50:11 -0600

    Adds two StudentCourse items to assign the courses to the student.

--- spec/features/students/show_spec.rb
+++ spec/features/students/show_spec.rb
@@ -3,9 +3,20 @@ require 'rails_helper'
 RSpec.describe 'As a visitor' do
   describe 'when I visit the each student page' do
     before(:each) do
-      @harry = Student.create(name: 'Harry Potter', age: 14, house: 'Gryffindor')
+      @harry = Student.create(
+        name: 'Harry Potter',
+        age: 14,
+        house: 'Gryffindor')
       @dada = Course.create(name: 'Defense Against the Dark Arts')
       @potions = Course.create(name: 'Potions')
+      harry_dada = StudentCourse.create(
+        student_id: @harry.id,
+        course_id: @dada.id
+      )
+      harry_potions = StudentCourse.create(
+        student_id: @harry.id,
+        course_id: @potions.id
+      )
 
     end
     it "I see a list of the student's courses" do
