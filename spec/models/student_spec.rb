9f9fc1d99d945412845edaf2e6ca4fc9eb3aa9c2
Kate Tester <katemorris@gmail.com>
2020-10-15 21:14:03 -0600

    Adds test for new student class method.

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,4 +13,14 @@ RSpec.describe Student, type: :model do
     it {should have_many(:courses).through(:student_courses)}
   end
 
+  describe 'class methods' do
+    it "average_age" do
+      harry = Student.create(name: 'Harry Potter', age: 14, house: 'Gryffindor')
+      draco = Student.create(name: 'Draco Malfoy', age: 14, house: 'Slytherin')
+      luna = Student.create(name: 'Luna', age: 13, house: 'Ravenclaw')
+
+      expect(Student.average_age).to eq(13.67)
+    end
+  end
+
 end
